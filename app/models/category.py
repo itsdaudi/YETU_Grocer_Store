dd7d4e7668dd1d7763eb729d12f58bcc9f75e537
itsdaudi <daudikombe77@gmail.com>
2026-07-18 19:05:11 +0300

    feat:adding app/models/category.py app//__init__.py.add Category model

---
+++ app/models/category.py
@@ -0,0 +1,14 @@
+from app.models import db
+
+
+class Category(db.Model):
+    __tablename__ = "categories"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    slug = db.Column(db.String(80), unique=True, nullable=False)
+
+    products = db.relationship("Product", backref="category", lazy=True)
+
+    def __repr__(self):
+        return f"<Category {self.name}>"
