e23cf17ab826b0278ae244c1bb6d4b1570430da8
itsdaudi <daudikombe77@gmail.com>
2026-07-18 18:09:28 +0300

    feat:avoiding circular imports by using init

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,25 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_migrate import Migrate
+from app.models import db
 
-@app.route("/")
-def index():
-    return jsonify({"message": "Yetu Grocer Store API"})
+migrate = Migrate()
+
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, origins=["http://localhost:5000"])
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///yetu.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    CORS(app, origins=["http://localhost:5173"])
+
+    @app.route("/")
+    def index():
+        return jsonify({"message": "Yetu Grocer Store API"})
 
     @app.route("/api/health")
     def health():
