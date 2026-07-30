f365d9961b615827274a270d4b2f9d23a55ee4ca
itsdaudi <daudikombe77@gmail.com>
2026-07-30 08:14:21 +0300

    feat:adding auth app

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -3,7 +3,6 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
-
 from app.models import db
 from app.models.user import User
 
@@ -96,3 +95,6 @@ def me():
             "role": user.role
         }
     }), 200
+
+
+
